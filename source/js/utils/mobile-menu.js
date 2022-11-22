60aa1a60fe78fe24d53208d6a31abc486fa32b88
Юрий Стецкий <yuraha15@yandex.ru>
2022-11-22 21:59:35 +0200

    now we can close mob menu by clicking everywhere

--- source/js/utils/mobile-menu.js
+++ source/js/utils/mobile-menu.js
@@ -3,7 +3,7 @@ function mobileMenu() {
   const menuLink = document.querySelectorAll('.menu__link');
   const body = document.querySelector('body');
   const headerBody = document.querySelector('.header__body');
-  const introText = document.querySelector('.intro__text');
+  const overlay = document.querySelector('.overlay');
 
   if (body.classList.contains('no-js')) {
     body.classList.remove('no-js');
@@ -12,19 +12,21 @@ function mobileMenu() {
   headerBurger.addEventListener('click', function () {
     headerBody.classList.toggle('header-menu--is-active');
     body.classList.toggle('scroll-lock');
+    overlay.classList.toggle('visually-hidden');
     // buttonUp.toggleAttribute("hidden");
   });
 
+  overlay.addEventListener('click', function () {
+    headerBody.classList.remove('header-menu--is-active');
+    body.classList.remove('scroll-lock');
+    overlay.classList.add('visually-hidden');
+  });
   for (let i = 0; i < menuLink.length; i++) {
     menuLink[i].addEventListener('click', function () {
       headerBody.classList.remove('header-menu--is-active');
       body.classList.remove('scroll-lock');
       // buttonUp.removeAttribute("hidden");
     });
-  }
-  if (headerBody.classList.contains('header-menu--is-active')) {
-    console.log('123');
-    // introText.classList.add('visually-hidden');
   }
 }
 
